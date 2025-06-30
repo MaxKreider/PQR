d177076616b4c085fd8f6d17e12a1015d97a463c
Peter Thomas CWRU <pjthomas@case.edu>
2025-06-29 22:15:29 -0400

    PJT edited cyclostationary_v_nice.m

--- cyclostationary_v_nice.m
+++ cyclostationary_v_nice.m
@@ -1,10 +1,15 @@
-%we consider the model
+% cyclostationary_v_nice.m
+%
+% Matrix Continued Fraction method for a periodically forced
+% stochastic ring model with a cyclostationary distribution.
+%
+% We consider the model
 %
 %  dx = [omega + epsilon*cos(x) + alpha*sin(t+phi) + beta*sin(2*t)]dt
 %           + sqrt(2D)dW(t)
 %
-%and use the matrix continued fraction method to solve for the
-%cyclostationary distribution.
+% and use the matrix continued fraction method to solve for the
+% cyclostationary distribution.
 
 %% setup
 
@@ -16,23 +21,30 @@ cutoff = 20;
 cutoff = cutoff*2+1;
 
 %mesh for eigenfunction
-dt = 0.05;
-X = 0:dt:2*pi;
-Y = X';
+% PJT modifications: Never use square grids for different quantities!  How
+% can you be sure you don't exchange them by mistake?
+dx = 2*pi/200;    
+dt = 2*pi/201;
+X = 0:dx:2*pi;
+Y = (0:dt:2*pi)';
+%dt = 0.05;
+%X = 0:dt:2*pi;
+%Y = X';
 
 %system parameters
 D = .01;
 omega = 1;
 epsilon = .2;
 alpha = .5;
-beta = .5;
-
+%beta = .5; % for alpha=beta we don't see a strong effect :-(
+beta=1;
+ 
 %cycle through phase offset
-phi = 0:0.1:2*pi;
+%phi = linspace(0,2*pi,3); % for debugging
+phi = linspace(0,2*pi,41); % for a finer mesh
 
 %firing rate solution matrix
-J_matrix = zeros(length(phi), length(Y));
-
+J_matrix = nan(length(phi), length(Y));
 
 %% explicit matrix method
 
@@ -87,8 +99,8 @@ for pp=1:length(phi)
     xi = xi(:,index)/max(xi(:,index));
 
     %evaluate
-    cyclo = zeros(length(X),length(Y));
-    basis = zeros(cutoff,cutoff);
+    cyclo = nan(length(X),length(Y));
+    basis = nan(cutoff,cutoff);
     for p=1:length(X)
         for q=1:length(Y)
 
@@ -107,7 +119,11 @@ for pp=1:length(phi)
 
     %Matlab indexing is dumb, and normalize
     cyclo = cyclo.';
-    cyclo = cyclo/(sum(sum(cyclo)*dt*dt));
+    cyclo = cyclo/(sum(sum(cyclo)*dt*dt)); % PT comment: is this the correct normalization?
+    % Shouldn't we go row-by-row?  Like this:
+    %for i=1:length(Y)
+    %    cyclo(i,:)=cyclo(i,:)/sum(cyclo(i,:); % normalize across x at each t
+    %end
 
     %prune imaginary part
     cyclo = real(cyclo);
@@ -141,7 +157,8 @@ for pp=1:length(phi)
     %% extract firing rate, i.e., J(x,t) at x = 2*pi
 
     %compute x-derivative
-    [dx,~] = gradient(cyclo, dt);
+    %[dx,~] = gradient(cyclo, dt);% original
+    [dcyclodx,~] = gradient(cyclo, dx); % revised
 
     %evaluate drift term at x = 2pi
     x_val = 2*pi;
@@ -149,46 +166,82 @@ for pp=1:length(phi)
 
     %cyclo at the edge
     cyclo_edge = cyclo(:, end);
-    der_edge = dx(:, end);
+    %der_edge = dx(:, end); % original
+    der_edge = dcyclodx(:, end); % revised
 
     %flux
     J = a_t .* cyclo_edge - D * der_edge;
+    % PT: save the values!
+    J_matrix(pp,:)=J;
     
-    figure(4)
-    hold on
-    plot3(Y, phi(pp)*ones(size(Y)), J, 'k-', 'LineWidth', 1.5)
-    xlabel('time t')
-    ylabel('phase offset \phi')
-    zlabel('firing rate J(t;\phi)')
-    title('Firing rate')
-    view(45, 30)
-    box on
-    set(gca, 'fontsize', 12)
+    % figure(4)
+    % hold on
+    % plot3(Y, phi(pp)*ones(size(Y)), J, 'k-', 'LineWidth', 1.5)
+    % xlabel('time t')
+    % ylabel('phase offset \phi')
+    % zlabel('firing rate J(t;\phi)')
+    % title('Firing rate')
+    % view(45, 30)
+    % box on
+    % set(gca, 'fontsize', 12)
+    % 
+    % figure(5)
+    % hold on
+    % plot(phi(pp),mean(J),'k.','markersize',40)
+    % xlabel('phase offset \phi')
+    % ylabel('E(J)')
+    % title('Mean firing rate')
+    % box on
+    % axis square
+    % set(gca,'fontsize',12)
+    % 
+    % figure(6)
+    % hold on
+    % plot(phi(pp),max(J),'k.','markersize',40)
+    % xlabel('phase offset \phi')
+    % ylabel('max(J)')
+    % title('Max firing rate')
+    % box on
+    % axis square
+    % set(gca,'fontsize',12)
 
-    figure(5)
-    hold on
-    plot(phi(pp),mean(J),'k.','markersize',40)
-    xlabel('phase offset \phi')
-    ylabel('E(J)')
-    title('Mean firing rate')
-    box on
-    axis square
-    set(gca,'fontsize',12)
-
-    figure(6)
-    hold on
-    plot(phi(pp),max(J),'k.','markersize',40)
-    xlabel('phase offset \phi')
-    ylabel('max(J)')
-    title('Max firing rate')
-    box on
-    axis square
-    set(gca,'fontsize',12)
-
-    pp
+    disp(['Completed phase value number ',num2str(pp),' out of ',num2str(length(phi))])
 
 end
 
+%% Display results
+
+figure
+T=Y;
+surf(T,phi,J_matrix)
+shading flat
+xlabel('Time t')
+ylabel('Phase offset \phi')
+title('Firing rate f(t,\phi)')
+set(gca,'FontSize',20)
+colormap hot
+
+figure
+TTT=[T;T+2*pi;T+4*pi;T+6*pi;T+8*pi]; % five periods
+JJJ=[J_matrix,J_matrix,J_matrix,J_matrix,J_matrix];
+%JJJ=[J_matrix(:,1:end-1),J_matrix(:,1:end-1),J_matrix(:,1:end-1),J_matrix(:,1:end-1),J_matrix(:,1:end-1)];
+for pp=1:2:length(phi)
+    subplot(1,2,1)
+    plot(TTT,pp+alpha*sin(TTT + phi(pp)) + beta*sin(2*TTT))
+    hold on
+    subplot(1,2,2)
+    plot(TTT,pp+JJJ(pp,:))
+    hold on
+end
+subplot(1,2,1)
+ylabel('Phase offset \phi')
+xlabel('Time')
+title('Input I(t)')
+set(gca,'FontSize',20)
+subplot(1,2,2)
+title('Firing Rate')
+xlabel('Time')
+set(gca,'FontSize',20)
 
 %% functionals to make my life easier
 
